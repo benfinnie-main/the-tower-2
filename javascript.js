278e8dbe95a31d75fcd6d625046eb8a59b21e22a
benfinnie-main <48133628+benfinnie-main@users.noreply.github.com>
2025-07-25 14:24:02 +0100

    test js functions

--- javascript.js
+++ javascript.js
@@ -7,6 +7,21 @@ console.log(favouriteAnimal("Rabbit"))
 const textBox = document.querySelector("#textBox");
 const output = document.querySelector("#output");
 
-textBox.addEventListener("keydown", (event) => {
-  output.textContent = `You pressed "${event.key}".`;
-});
+textBox.addEventListener("change", () => {
+  let num = parseFloat(textBox.value);
+  const original = num;
+  if (isNaN(num)) {
+    output.textContent = `Please enter a number`
+  } else {
+    if (num < 0) output.textContent = (`Please enter a positive number`);
+    if (num === 0) output.textContent = (`${original}! equals 1`);;
+    if (num >= 1) {
+      let x = num - 1;
+      while (x > 1) {
+        num *= x;
+        x--;
+      }
+      output.textContent = (`${original}! equals ${num}`);
+    }
+    }
+  });
